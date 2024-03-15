8cd09d3566d24a74f8f5125f447cf7356a448b4f
Suryakant <skant758@gmail.com>
2024-03-15 17:23:44 +0530

    added path alias support

--- app/routes/AuthRoute.tsx
+++ app/routes/AuthRoute.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import GetStartedScreen from "../screens/GetStartedScreen";
-import SignInScreen from "../screens/SignInScreen";
-import SignUpScreen from "../screens/SignUpScreen";
+import GetStartedScreen from "@screens/GetStartedScreen";
+import SignInScreen from "@screens/SignInScreen";
+import SignUpScreen from "@screens/SignUpScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 const AuthRoute = () => {
 	return (
-		<Stack.Navigator>
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
 			<Stack.Screen name='GetStarted' component={GetStartedScreen} />
 			<Stack.Screen name='SignIn' component={SignInScreen} />
 			<Stack.Screen name='SignUp' component={SignUpScreen} />
