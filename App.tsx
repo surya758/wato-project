0ffac8f0398a567e34752ae7d3177b11313d5599
Suryakant <skant758@gmail.com>
2024-03-15 16:13:51 +0530

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded, fontError] = useFonts({
+		"GT-Super-Text-Regular-Trial": require("./app/assets/fonts/GT-Super-Text-Regular-Trial.otf"),
+		"Inter-Regular": require("./app/assets/fonts/Inter-Regular.ttf"),
+		"Sequel-Sans-Black-Body": require("./app/assets/fonts/Sequel Sans Black Body.ttf"),
+	});
+
+	if (!fontsLoaded && !fontError) {
+		return <Text> Loading... </Text>;
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>Hello</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ app/routes/RootRoute.ts
@@ -0,0 +1,6 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RootRoute = () => { }
+
+export default RootRoute;
