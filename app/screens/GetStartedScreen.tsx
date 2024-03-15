4ef64885172e360737f34f3d303e9873d3a886e6
Suryakant <skant758@gmail.com>
2024-03-15 23:08:45 +0530

    get started page completed

--- app/screens/GetStartedScreen.tsx
+++ app/screens/GetStartedScreen.tsx
@@ -1,43 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, Pressable, ImageBackground } from "react-native";
+import React, { useCallback } from "react";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
-import { LinearGradient } from "expo-linear-gradient";
 
 import { AuthStackParamList } from "@routes/AuthRoute";
 import { colors, images, fonts } from "@themes";
 import Layout from "@atoms/Layout";
+import GradientButton from "@molecules/GradientButton";
 
 type GetStartedScreenNavigationProp = NativeStackNavigationProp<AuthStackParamList, "GetStarted">;
 
 const GetStartedScreen = () => {
 	const navigation = useNavigation<GetStartedScreenNavigationProp>();
+
+	const navigateToSignUp = () => navigation.navigate("SignUp");
+
 	return (
 		<Layout>
-			<View style={styles.upperContainer}>
+			<View style={styles.upperContainer} />
+			<View style={styles.lowerContainer}>
 				<View>
-					<Text style={styles.title}>wato</Text>
-					<Text style={styles.subtitle}>{`Zero Cost\nWhatsApp Messaging\nplatform`}</Text>
+					<Text style={styles.titleText}>wato</Text>
+					<Text style={styles.subtitleText}>{`Zero Cost\nWhatsApp Messaging\nplatform`}</Text>
 				</View>
 
-				<View style={styles.lowerContainer}>
-					<ImageBackground source={images.grain}>
-						<LinearGradient
-							start={{ x: 0.7, y: 0 }}
-							end={{ x: 0, y: 0 }}
-							colors={[colors.gradientPrimary, colors.gradientSecondary]}
-							style={styles.gradientStyle}
-						>
-							<Pressable>
-								<Text style={styles.getStartedText}>Get Started</Text>
-							</Pressable>
-						</LinearGradient>
-					</ImageBackground>
+				<GradientButton navigateToSignUp={navigateToSignUp} />
+
+				<View style={styles.signInContainer}>
+					<Text style={styles.alreadyAccountText}>Already have an account? </Text>
+					<Pressable onPress={() => navigation.navigate("SignIn")}>
+						<Text style={styles.signInText}>Sign in instead</Text>
+					</Pressable>
 				</View>
 			</View>
 
-			<Image source={images.sparkles} style={styles.sparklesImageStyle} />
-			<Image source={images.rays} style={styles.raysImageStyle} resizeMode='stretch' />
+			<Image source={images.sparkles} style={styles.sparklesImage} />
+			<Image source={images.rays} style={styles.raysImage} resizeMode='stretch' />
 		</Layout>
 	);
 };
@@ -47,54 +45,35 @@ export default GetStartedScreen;
 const styles = StyleSheet.create({
 	upperContainer: {
 		flex: 1,
-		marginHorizontal: 20,
-		borderColor: "red",
-		justifyContent: "center",
 	},
-	sparklesImageStyle: {
+	lowerContainer: {
+		flex: 1.5,
+		marginHorizontal: 20,
+	},
+	sparklesImage: {
 		width: "100%",
 		height: "60%",
 		position: "absolute",
 		opacity: 0.4,
 		transform: [{ rotate: "30deg" }],
 	},
-	raysImageStyle: {
+	raysImage: {
 		width: "100%",
 		height: "60%",
 		position: "absolute",
 	},
-	title: {
+	titleText: {
 		fontSize: fonts.h1,
 		color: colors.white,
 		fontFamily: fonts.heading1,
 	},
-	subtitle: {
+	subtitleText: {
 		marginTop: 24,
 		fontSize: fonts.h2,
 		color: colors.white,
 		fontFamily: fonts.heading2,
 	},
-	lowerContainer: {
-		overflow: "hidden",
-		borderWidth: 1,
-		borderColor: "white",
-		borderRadius: 10,
-		marginTop: 74,
-		elevation: 2,
-		shadowColor: colors.gradientPrimary,
-		shadowOffset: { height: 6, width: 6 },
-		shadowOpacity: 0.6,
-		shadowRadius: 4,
-	},
-	gradientStyle: {
-		borderRadius: 10,
-		opacity: 0.9,
-	},
-	getStartedText: {
-		marginVertical: 13,
-		textAlign: "center",
-		fontFamily: fonts.regular,
-		fontSize: fonts.h3,
-		color: colors.white,
-	},
+	signInContainer: { flexDirection: "row", justifyContent: "center", marginTop: 20 },
+	alreadyAccountText: { color: colors.white, fontFamily: fonts.primary, opacity: 0.7 },
+	signInText: { color: colors.primary },
 });
